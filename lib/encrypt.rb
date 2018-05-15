bfa18d080bfaf29e4a70e967471f00ddf0441f24
Erik Schneider <erikschneider2211@gmail.com>
2018-05-15 15:11:15 -0600

    add encrypt file and pseudocode

---
+++ lib/encrypt.rb
@@ -0,0 +1,13 @@
+class Encrypt
+
+ # grab random key number
+ # grab offset number
+ # grab message
+ # change message input from string to array for each element
+ # add offset and rotation to get a number
+ # shift letter by appropriate number of rotations and offsets
+ # determine correct starting point for rotations and offsets
+ # iterate through entire message and place in local variable
+ # Turn message into a string
+ # return encrypted message in encrypted format
+ 
