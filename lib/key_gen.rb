2423293d36b17a8ef27347e102aa95d307b53674
Harper Bellows <harper.bellows@gmail.com>
2018-05-15 15:05:43 -0600

    add offset to key_gen

--- lib/key_gen.rb
+++ lib/key_gen.rb
@@ -3,6 +3,8 @@ attr_accessor  :key
 
   def initialize
     @key = rand(10000..99999).digits.reverse
+    @date = Date.new(2018,5,14).strftime('%e%m%y')
+    #is it really necessary to reverse these again?
   end
 
   def rotation_a
@@ -24,4 +26,31 @@ attr_accessor  :key
     d = @key
     d[3..4].join.to_i
   end
+
+  def square_the_date
+    date = @date.to_i
+    date ** 2
+  end
+
+  def offset_digits
+    date = square_the_date.to_s.split('')
+    date[-4..-1]
+  end
+
+  def offset_a
+    offset_digits[0].to_i
+  end
+
+  def offset_b
+    offset_digits[1].to_i
+  end
+
+  def offset_c
+    offset_digits[2].to_i
+  end
+
+  def offset_d
+    offset_digits[3].to_i
+  end
+  
 end
