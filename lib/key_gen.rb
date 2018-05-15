9d8a611b7ba7bee4f782b2ebd36cdf93547a27c4
Harper Bellows <harper.bellows@gmail.com>
2018-05-15 12:41:14 -0600

    add key gen refactor

--- lib/key_gen.rb
+++ lib/key_gen.rb
@@ -2,26 +2,26 @@ class KeyGenerator
 attr_accessor  :key
 
   def initialize
-    @key = rand(10000..99999).to_s.split('').map(&:to_i)
+    @key = rand(10000..99999).digits.reverse
   end
 
   def rotation_a
     a = @key
-    (a[0] * 10) + a[1]
+    a[0..1].join.to_i
   end
 
   def rotation_b
     b = @key
-    (b[1] * 10) + b[2]
+    b[1..2].join.to_i
   end
 
   def rotation_c
     c = @key
-    (c[2] * 10) + c[3]
+    c[2..3].join.to_i
   end
 
   def rotation_d
     d = @key
-    (d[3] * 10) + d[4]
+    d[3..4].join.to_i
   end
 end
