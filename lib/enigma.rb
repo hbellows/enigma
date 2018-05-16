7b84ef349e3859ce2344fae52030bbf22239f73b
Harper Bellows <harper.bellows@gmail.com>
2018-05-16 08:23:48 -0600

    add enigma source and test files

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,13 +1,17 @@
 require 'date'
 
 class Enigma
-  
+
   def initialize(key = rand(10000..99999).digits.reverse,
                  date = Date.new.strftime('%e%m%y'))
-                 
-    @key = key 
-    @date = date             
-  end
- 
 
-end 
+    @key = key
+    @date = date
+  end
+
+  def first_method
+
+  end
+
+
+end
---
+++ test/enigma_test.rb
@@ -0,0 +1,8 @@
+require './test/test_helper'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/encrypt'
+
+class EnigmaTest < Minitest::Test
+
+end 
