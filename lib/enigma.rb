08462bdc13417ac762490db99625931f30d53b18
Harper Bellows <harper.bellows@gmail.com>
2018-05-16 12:54:49 -0600

    remove deleted files from remote

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,16 +1,19 @@
 require 'date'
 
 class Enigma
+  attr_reader :file
 
   def initialize(key = rand(10000..99999).digits.reverse,
                  date = Date.new.strftime('%e%m%y'))
 
     @key = key
     @date = date
+    @file = File.read(ARGV[0])
   end
 
-  def first_method
-
+  def encrypt 
+    encrypt = Encrypt.new(key, date)
+    encrypted = encrypt.encrypt(file)
   end
 
 
